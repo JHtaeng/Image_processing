e89c8c4b5e6e9604196e47c798755f4ccec59712
JHtaeng <68258591+JHtaeng@users.noreply.github.com>
2020-08-07 09:53:03 +0900

    web server

--- server.py
+++ server.py
@@ -2,7 +2,9 @@
 import cv2
 import numpy as np
 import datetime
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
+
+image = None 
 
 app = Flask(__name__)
 
@@ -32,42 +34,55 @@ def chromakey(img, background):
 
 @app.route('/')
 def index():
-    html = """
-        <form action=/upload method=post enctype='multipart/form-data'>
-            <input type=file name=file1> <input type=submit value='전송'>
-         <select name=backimg>
-             <option value=bg1.jpg>대나무</option>
-             <option value=bg2.jpg>상가</option>
-             <option value=bg3.jpg>TV</option>
-         </select>
-         <img src=/static/bg1.jpg width=100>
-         <img src=/static/bg2.jpg width=100>
-         <img src=/static/bg3.jpg width=100>
-        </form>
-    
-    """
-    return html
+   
+    return render_template("imageprocessing.html", ctx={"title":"영상처리"})
 
-@app.route('/upload', methods=["post"])
+@app.route('/upload', methods=["POST"])
 def upload():
+    global image
     f = request.files['file1']
     filename = "./static/" + f.filename
     f.save(filename)
     
-    backimg = request.form.get("backimg")
-    print('slslsl')
-    print(backimg)
+    image = cv2.imread(filename)
+    cv2.imwrite('./static/result.jpg', image)
+    print(image.shape)
     
-    img = cv2.imread(filename)
-    img = cv2.resize(img, dsize=(320,240))
+    return redirect("/")
+
+@app.route('/imageprocess')
+def imageprocess():
+    global image
+    method = request.args.get("method")
+    if method == "emboss":
+        print(image.shape)
+        print("emboss")
+        
+        emboss = np.array([[-1,-1,0],
+                           [-1,0,1],
+                           [0,1,1]], np.float32)
+
+        dst = cv2.filter2D(image, -1,emboss, delta=128)
+        cv2.imwrite("./static/result.jpg", np.vstack((image,dst)))
+        
+        
+    if method == "sharp":
+        print(image.shape)
+        
+        sharp = np.array([[0,-1,0],
+                           [-1,5,-1],
+                           [0,-1,0]], np.float32)
+
+        dst = cv2.filter2D(image, -1, sharp)
+        cv2.imwrite("./static/result.jpg", np.vstack((image,dst)))
+        
+        
+    if method == "blur":
+        size = int(request.args.get("size",3))
+        dst = cv2.blur(image,(size,size))
+        cv2.imwrite("./static/result.jpg", np.vstack((image,dst)))
     
-    bg = cv2.imread('./static/' + backimg)
-    bg = cv2.resize(bg, dsize=(320,240))
-    
-    img = chromakey(img, bg)
-    cv2.imwrite(filename, img)
-    
-    return "<img src=/static/" + f.filename + "?"+ datetime.datetime.now().strftime('%H%M%S') + ">"
+    return "hello!~@~!@~!@~!@~!@~!@~"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
