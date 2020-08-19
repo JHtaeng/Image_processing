f8b66e99cc2a8219296b98cc64f292a1ec70ce38
JHtaeng <68258591+JHtaeng@users.noreply.github.com>
2020-08-19 13:57:21 +0900

    web server

--- server.py
+++ server.py
@@ -4,50 +4,41 @@ from flask import Flask,render_template,url_for,request
 import base64
 import numpy as np
 import cv2
-from keras.models import load_model
-import io
+from keras.models import load_model 
+import io 
+import json
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input
 
-init_Base64 = 21   # data:image/png;base64, 로 시작하
+
+init_Base64 = 22   # data:image/png;base64, 로 시작하
 app = Flask(__name__)
-model = load_model('mnist_cnn_tiny.h5')
-modelH = load_model('hand_written_korean_classification.hdf5')
-
-labels_file = io.open("label.txt",'r', encoding='utf-8').read().split()
-label = [str for str in labels_file]
-
 
 @app.route('/')
 def home():
-    return render_template("mnist.html")
+    return "ok~~~"
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    
-    draw = request.form['url']        # hidden 변수로 전달함            
+@app.route('/image', methods=['POST'])
+def upload():       
+    draw = request.form['photo_cap']
     draw = draw[init_Base64:]
     draw_decoded = base64.b64decode(draw)
     image = np.asarray(bytearray(draw_decoded), dtype="uint8")
     
-    mode = request.form.get("mode","digit")
-    if mode == "digit":
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-        image = cv2.resize(image, dsize=(28,28), interpolation=cv2.INTER_AREA)
-        image = image.reshape(1,28,28,1)
-        
-        p = model.predict(image)
-        p = np.argmax(p)
-        
-    else:
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-        image = cv2.resize(image, dsize=(32,32), interpolation=cv2.INTER_AREA)
-        image = (255-image)/255
-        image = image.reshape(1,32,32,3)
-        p = modelH.predict(image)
-        p = label[np.argmax(p)]
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)    
+    image = cv2.resize(image, dsize=(224, 224), interpolation=cv2.INTER_AREA) 
+    image = image.reshape(-1,224,224,3)
+    
+    model = ResNet50(weights='imagenet')
+    
+    image=preprocess_input(image)
+    pred = model.predict(image)
+    
+    json_data = json.load(open('imagenet_class_index.json'))
+    r = json_data[f"{np.argmax(pred)}"][1]
 
-    return f"result:{p} <a href=javascript:history.back()>뒤로</a>"
-
+    return f"인식 결과: {r}"
+    
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', debug=True, port=8000)
